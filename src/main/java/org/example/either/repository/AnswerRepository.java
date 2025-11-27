d6e86aed0261cb8cffd33f0c0026def930c801f1
ohmyeongje <tiqmtiqm6541@gmail.com>
2025-11-27 11:46:46 +0900

    answer/question 레파지토리

---
+++ src/main/java/org/example/either/repository/AnswerRepository.java
@@ -0,0 +1,11 @@
+package org.example.either.repository;
+
+import org.example.either.entity.Answer;
+import org.example.either.entity.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    long countByQuestionAndAnswerText(Question q, String text);
+}
