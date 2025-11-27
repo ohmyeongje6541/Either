e225f2536d5082b5a03d257479d2f6fcab9512f7
ohmyeongje <tiqmtiqm6541@gmail.com>
2025-11-27 11:48:29 +0900

    question 컨트롤러 로직 구현에 따른 파일

---
+++ src/main/resources/templates/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO question (title, option_a, option_b)
+VALUES ('치킨 vs 피자', '치킨', '피자');
+
+INSERT INTO question (title, option_a, option_b)
+VALUES ('여름 vs 겨울', '여름', '겨울');
+
+INSERT INTO question (title, option_a, option_b)
+VALUES ('바다 vs 산', '바다', '산');
