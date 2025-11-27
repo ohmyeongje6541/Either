671162f02308917159697c4ebcf48a2ed0f3b76c
ohmyeongje <tiqmtiqm6541@gmail.com>
2025-11-27 11:45:53 +0900

    project init

---
+++ src/main/java/org/example/either/EitherApplication.java
@@ -0,0 +1,13 @@
+package org.example.either;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EitherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EitherApplication.class, args);
+    }
+
+}
